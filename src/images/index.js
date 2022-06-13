f21efb04d036d469d2217ad80d95b5f7f53e95de
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2022-06-13 13:51:09 -0500

    Deploy React app to GitHub Pages

--- src/images/index.js
+++
@@ -1,9 +0,0 @@
-
-
-const IMAGES =  {
-        logo : require('./logo.webp'),
-        aboutUs1 : require('./about-us-1.jpg'),
-        aboutUs2 : require('./about-us-2.jpg'),
-    }
-  
-export default IMAGES
